7d94c02bd2e8dea4223ea0347e4fb2b2377e38b1
wemigliari <wemigliari@yahoo.com.br>
2024-11-05 21:03:41 +0100

    First commit

---
+++ javascript/footer.js
@@ -0,0 +1,89 @@
+/**
+ * @Author: wellingtonmigliari
+ * @Date:   2022-10-11T17:26:33+02:00
+ * @Last modified by:   wellingtonmigliari
+ * @Last modified time: 2024-02-10T01:39:37+01:00
+ */
+
+footer.innerHTML = `
+<footer>
+   <div class="footer">
+      <div class="container">
+         <div class="row">
+            <div class="col-md-12">
+               <a class="logo_footer"><img src="images/crisp_logo_novo.png" alt="#"/></a>
+            </div>
+            <div class="col-md-5">
+               <div class="Informa conta">
+                  <h3>endereço</h3>
+                  <ul>
+                     <li>Av. Presidente Antônio Carlos, nº 6627 <br>
+                       Pampulha - Unidade Administrativa III<br>
+                       Universidade Federal de Minas Gerais <br>
+                     </li>
+                  </ul>
+               </div>
+               <!--
+               <div class="Informa helpful">
+                  <ul>
+                     <li><a href="index.html">início</a></li>
+                     <li><a href="projeto.html">projeto</a></li>
+                     <li><a href="dados.html">dados</a></li>
+                     <li><a href="equipe.html">mapas</a></li>
+                     <li><a href="estudos.html">estudos</a></li>
+                  </ul>
+               </div>
+             -->
+            </div>
+            <div class="col-md-4">
+               <div class="Informa conta">
+                  <h3>contato</h3>
+                  <ul>
+                     <li>Tel.: (31) 3409-6310/3409-6306
+                     </li>
+                     <li>E-mail: crisp@crisp.ufmg.br
+                     </li>
+                  </ul>
+               </div>
+               <ul class="social_icon text_align_center">
+                 <li> <a href="https://github.com/wemigliari"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+                  <li> <a href="https://www.facebook.com/CrispUFMG/"><i class="fa fa-facebook"></i></a></li>
+                  <li> <a href="https://www.instagram.com/crisp.ufmg/"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                  <li> <a href="https://open.spotify.com/show/08kfV9vcmBXKh8CQ3n8zaH"><i class="fa-brands fa-spotify"></i></a></li>
+               </ul>
+            </div>
+            <div class="col-md-3">
+              <!--
+               <div class="Informa">
+                  <h3>Newsletter</h3>
+                  <form class="newslatter_form">
+
+                     <input class="ente" placeholder="Digite seu e-mail" type="text" name="email" style="color: #c37159;">
+                     <a href="index.html#newsletter"><button class="subs_btn">Inscrever-me</button></a>
+                  </form>
+               </div>
+               -->
+            </div>
+         </div>
+      </div>
+      <div class="copyright text_align_center" style="background-color: #ddd9d2;">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-10 offset-md-1">
+                 <div class="Informa helpful">
+                    <ul>
+                       <li><a href="index.html">início</a></li>
+                       <li><a href="projeto.html">projeto</a></li>
+                       <li><a href="dados.html">dados</a></li>
+                       <li><a href="equipe.html">equipe</a></li>
+                       <li><a href="estudos.html">publicações</a></li>
+                    </ul>
+                 </div>
+                  <p>© 2023-2025 Todos os direitos reservados <br>Criado por W. Migliari</a></p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</footer>
+`;
